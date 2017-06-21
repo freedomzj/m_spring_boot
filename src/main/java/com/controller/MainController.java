47f44503564c09cba05c6ac24d7b87c7f108bd68
曾杰 <zengjie@cengjiedeMac-mini.local>
2017-06-21 14:56:55 +0800

    更改map迭代

--- src/main/java/com/controller/MainController.java
+++ src/main/java/com/controller/MainController.java
@@ -21,15 +21,11 @@ public class MainController {
     @RequestMapping(value={"/index","/"}, method= RequestMethod.GET)
     public String main(Model model, HttpServletRequest request){
         User u=new User(1,"zengjie","123456");
-        User u1=new User(2,"zengjie2","1234561");
-        User u2=new User(3,"zengjie3","1234561");
         List<User> users=new ArrayList<User>();
-        Map<String,Object> map=new HashMap<String,Object>();
-        map.put("userMap",users);
-        map.put("username","曾杰");
         users.add(u);
-        users.add(u1);
-        users.add(u2);
+        Map<String,Object> map=new HashMap<String,Object>();
+        map.put("password","123456");
+        map.put("username","曾杰");
         model.addAttribute("dataMap",map);
         model.addAttribute("users",users);
         model.addAttribute("username","zengjie");
